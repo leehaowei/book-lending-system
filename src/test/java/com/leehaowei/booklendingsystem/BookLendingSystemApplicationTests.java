809636ae2572aefecffa09aaddf0432660b8735d
Haowei Lee <haoweilee.tpe@gmail.com>
2023-04-19 06:41:03 +0200

    initial commit

---
+++ src/test/java/com/leehaowei/booklendingsystem/BookLendingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leehaowei.booklendingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLendingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
